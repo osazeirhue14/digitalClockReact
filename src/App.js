bb9a57d1f538591c9dd14d70277bc31ae3edaa29
Osaze <osaze.irhue.2022@mumail.ie>
2024-06-23 19:09:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+//import logo from './mountain.jpg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+  const [time,setTime]=useState((new Date()));
+  useEffect(() => {
+             const intId = setInterval(() =>{
+             setTime((new Date()));
+           },1000);
+  
+  return () =>{
+    clearInterval(intId);
+  }        
+},[]);
+function formatTime(){
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  let meridian = hours <= 12 ? 'AM' : 'PM';
+
+  hours = hours%12||12;
+
+  return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(seconds)} ${meridian}`;
+
+}
+function padZeros(number){
+  return number < 10 ? '0' + number : number;
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="digital-clock">
+      <span>{formatTime()}</span>
+
+     
+        
+        
+     
     </div>
   );
 }
